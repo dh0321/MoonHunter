1aa951a2ea892c1f2a5f8a2967855523393a91f6
dh0321 <111106711+dh0321@users.noreply.github.com>
2023-04-28 02:44:54 +0900

    Using PrimaryDataAsset of Shoulder and Quater view

---
+++ Source/MoonHunterProject/Private/Characters/MHCharacterControlData.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/MHCharacterControlData.h"
+
+UMHCharacterControlData::UMHCharacterControlData()
+{
+	TargetArmLength = 500;
+}
